ab0f972b27207e03dc6da5a0ab0a3943705f1a35
andyandrwew <andyandrwew144@gmail.com>
2017-07-03 09:50:08 -0400

    Signed-off-by: andyandrwew <andyandrwew144@gmail.com>

---
+++ src/finalProject/LinearSearch.java
@@ -0,0 +1,19 @@
+package finalProject;
+
+class LinearSearch
+{
+    // This function returns index of element x in arr[]
+    static int search(int arr[], int x)
+    {
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            // Return the index of the element if the element
+            // is found
+            if (arr[i] == x)
+                return i;
+        }
+  
+        // return -1 if the element is not found
+        return -1;
+    }
+} 
---
+++ src/finalProject/MainTester.java
@@ -0,0 +1,88 @@
+package finalProject;
+
+import java.util.Random;
+
+public class MainTester
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int[] list = new int[100001];
+		int[] list1 = list;
+		int[] list2 = list;
+		
+		Random r = new Random();
+		
+		BubbleSort bubbleSort = new BubbleSort();
+		MergeSort mergeSort = new MergeSort();
+		InsertionSort insertionSort = new InsertionSort();
+		
+		BinarySearch binarySearch = new BinarySearch();
+		LinearSearch linearSearch = new LinearSearch();
+		
+		System.out.println("Building data..." + "\n");
+		
+		for(int i = 0; i < 100000; i++)
+		{
+			list[i] = r.nextInt();
+		}
+		
+		for(int i = 0; i < 100000; i++)
+		{
+			list1[i] = list[i];
+			list2[i] = list[i];
+			
+		}
+		
+		list[100000] = 900;
+		list1[100000] = 900;
+		list2[100000] = 900;
+		
+		System.out.println("Insertion Sort....");
+
+		
+		long timeB4 = System.currentTimeMillis();
+		insertionSort.sort(list1);
+		long timeAfter = System.currentTimeMillis();
+		System.out.println("Insertion Sort took: " + (timeAfter - timeB4) + "ms");
+		
+		
+		System.out.println("Bubble Sort....");
+		
+		timeB4 = System.currentTimeMillis();
+		bubbleSort.bubbleSort(list);
+		timeAfter = System.currentTimeMillis();
+		System.out.println("Bubble Sort took: " + (timeAfter - timeB4) + "ms");
+		
+		System.out.println("Merge Sort....");
+
+		
+		timeB4 = System.currentTimeMillis();
+		mergeSort.merge(list2, 0, list.length/2, list.length - 1);
+		timeAfter = System.currentTimeMillis();
+		System.out.println("Merge Sort took: " + (timeAfter - timeB4) + "ms" + "\n");
+		
+		
+		System.out.println("Binary Search....");
+
+		
+		timeB4 = System.currentTimeMillis();
+		System.out.println("Found at position " + binarySearch.binarySearch(list, 900));
+		timeAfter = System.currentTimeMillis();
+		System.out.println("Binary Search took: " + (timeAfter - timeB4) + "ms");
+		
+		
+		System.out.println("Linear Search....");
+
+		
+		
+		timeB4 = System.currentTimeMillis();
+		System.out.println("Found at position " + linearSearch.search(list, 900));
+		timeAfter = System.currentTimeMillis();
+		System.out.println("Linear Search took: " + (timeAfter - timeB4) + "ms");
+		
+		
+	}
+
+}
