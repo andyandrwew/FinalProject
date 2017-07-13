1de69560ff0972e00bf9dd7a491779439906ddaf
andyandrwew <andyandrwew144@gmail.com>
2017-07-13 17:55:22 -0400

    Signed-off-by: andyandrwew <andyandrwew144@gmail.com>

--- src/finalProject/MainTester.java
+++ src/finalProject/MainTester.java
@@ -11,17 +11,18 @@ public class MainTester
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		int[] list = new int[200000];
+		int arraySizes = 200000;
+		int position = -1;
+		int[] list = new int[arraySizes];
 		int[] list1 = list;
 		int[] list2 = list;
 		int[] list3 = list;
 		
 		
-		int searchNum = 9;
+		int searchNum = 100;
 		
-		int minRange = 0;
-		int maxRange = list.length;
+		int minRange = -4;
+		int maxRange = list.length - 4;
 			
 		
 		Random r = new Random();
@@ -31,54 +32,45 @@ public class MainTester
 		for(int i = 0; i < list.length; i++)
 		{
 			list[i] = r.nextInt((maxRange - minRange) + 1) + minRange;
-		}
-		
-		for(int i = 0; i < list.length; i++)
-		{
 			list1[i] = list[i];
 			list2[i] = list[i];
-			list3[i] = list[i];			
+			list3[i] = list[i];
 		}
 		
-		
 		System.out.println("Insertion Sort....");
-
 		
 		long timeB4 = System.currentTimeMillis();
-		InsertionSort.sort(list1);
+		InsertionSort.sort(list);
 		long timeAfter = System.currentTimeMillis();
 		System.out.println("Insertion Sort took: " + (timeAfter - timeB4) + "ms");
 		
+		System.out.println("Merge Sort....");
+		
+		timeB4 = System.currentTimeMillis();
+		MergeSort.sort(list1);
+		timeAfter = System.currentTimeMillis();
+		System.out.println("Merge Sort took: " + (timeAfter - timeB4) + "ms");
 		
 		System.out.println("Bubble Sort....");
 		
 		timeB4 = System.currentTimeMillis();
-		BubbleSort.bubbleSort(list);
+		BubbleSort.bubbleSort(list2);
 		timeAfter = System.currentTimeMillis();
-		System.out.println("Bubble Sort took: " + (timeAfter - timeB4) + "ms");
-		
-		System.out.println("Merge Sort....");
-
-		
-		timeB4 = System.currentTimeMillis();
-		MergeSort.sort(list2);
-		timeAfter = System.currentTimeMillis();
-		System.out.println("Merge Sort took: " + (timeAfter - timeB4) + "ms" + "\n");
-		
+		System.out.println("Bubble Sort took: " + (timeAfter - timeB4) + "ms");		
 		
 		System.out.println("Linear Search....");
 		
 		timeB4 = System.currentTimeMillis();
-		System.out.println("Found at position " + LinearSearch.search(list3, searchNum));
+		position = LinearSearch.search(list3, searchNum);
 		timeAfter = System.currentTimeMillis();
+		System.out.println("\n" + "Found at position " + position);
 		System.out.println("Linear Search took: " + (timeAfter - timeB4) + "ms");		
-		
 		
 		System.out.println("Binary Search....");
 		
 		timeB4 = System.currentTimeMillis();
 		Arrays.sort(list3);
-		int position = Arrays.binarySearch(list3, searchNum);
+		position = Arrays.binarySearch(list3, searchNum);
 		timeAfter = System.currentTimeMillis();
 		if (position < 0)
 		{
