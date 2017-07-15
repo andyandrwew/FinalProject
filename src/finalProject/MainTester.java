7a3227bf1942ca1f34bdc41ee6080df57eb95cf9
andyandrwew <andyandrwew144@gmail.com>
2017-07-15 19:57:04 -0400

    Signed-off-by: andyandrwew <andyandrwew144@gmail.com>

--- src/finalProject/MainTester.java
+++ src/finalProject/MainTester.java
@@ -5,21 +5,28 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class MainTester
 {
 
-	public static void main(String[] args)
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception
 	{
+		int arraySizes = 0;
+		int counterArray = 0;
+		Scanner in = new Scanner(System.in);
+		
 		//modify number of entries to sort and search through below
-		int arraySizes = 200000;
+		System.out.println("How many random numbers? \n(Warning over 200,000 may take very long)");
 		
+		while (counterArray == 0)
+		{
+			arraySizes = (int) in.nextInt();
+			counterArray++;
+		}
 		
-		long timeOfInsertion = 0;
-		long timeOfMerge = 0;
-		long timeOfLinear = 0;
-		long timeOfSelection = 0;
-		long timeOfBinary = 0;
+		long timeOfAction = 0;
 		long timeOfSort = 0;
 		long timeB4 = 0;
 		long timeAfter = 0;
@@ -30,19 +37,45 @@ public class MainTester
 		int[] list1 = list;
 		int[] list2 = list;
 		int[] list3 = list;
+		int[] list4 = list;
+		
 
 		
 		//modify the integer to search for below
-		int searchNum = 15000;
+		int searchNum = 0;
+		int counterSearch = 0;
+		System.out.println("What integer would you like to search for?");
 		
+		while (counterSearch == 0)
+		{
+			searchNum = (int) in.nextInt();
+			counterSearch++;
+		}
 		
 		//modify range of integers below 
-		int minRange = -4;
-		int maxRange = list.length - 4;
+		int minRange = 0;
+		int counterMin = 0;
+		System.out.println("What is the minimum range of the data?");
+		
+		while (counterMin == 0)
+		{
+			minRange = (int) in.nextInt();
+			counterMin++;
+		}
+		
+		int maxRange = 0;
+		System.out.println("What is the maximum range of the data?");
+		int counterMax = 0;
+
+		while (counterMax == 0)
+		{
+			maxRange = (int) in.nextInt();
+			counterMax++;
+		}
 			
 		Random r = new Random();
 		
-		System.out.println("Building data..." + "\n");
+		System.out.println("Building data...");
 		
 		for(int i = 0; i < list.length; i++)
 		{
@@ -50,6 +83,7 @@ public class MainTester
 			list1[i] = list[i];
 			list2[i] = list[i];
 			list3[i] = list[i];
+			list4[i] = list[i];
 		}
 		
 		System.out.println("\nInsertion Sort....");
@@ -57,40 +91,35 @@ public class MainTester
 		timeB4 = System.currentTimeMillis();
 		InsertionSort.sort(list2);
 		timeAfter = System.currentTimeMillis();
-		timeOfInsertion = timeAfter - timeB4;
-		System.out.println("Insertion Sort took: " + timeOfInsertion + "ms");
-		
+		timeOfAction = timeAfter - timeB4;
+		System.out.println("Insertion Sort took: " + timeOfAction + "ms");
 		
 		System.out.println("\nSelection Sort....");
 		
 		timeB4 = System.currentTimeMillis();
 		SelectionSort.sort(list);
 		timeAfter = System.currentTimeMillis();
-		timeOfSelection = timeAfter - timeB4;
-		System.out.println("Selection Sort took: " + timeOfSelection + "ms");
-		
-		
+		timeOfAction = timeAfter - timeB4;
+		System.out.println("Selection Sort took: " + timeOfAction + "ms");
 		
 		System.out.println("\nMerge Sort....");
 		
 		timeB4 = System.currentTimeMillis();
 		MergeSort.sort(list1);
 		timeAfter = System.currentTimeMillis();
-		timeOfMerge = timeAfter - timeB4;
-		System.out.println("Merge Sort took: " + timeOfMerge + "ms");
-		
+		timeOfAction = timeAfter - timeB4;
+		System.out.println("Merge Sort took: " + timeOfAction + "ms");
 				
-		
 		System.out.println("\nLinear Search....");
 		
 		timeB4 = System.currentTimeMillis();
 		position = LinearSearch.search(list3, searchNum);
 		timeAfter = System.currentTimeMillis();
 		System.out.println("Found at position " + position);
-		timeOfLinear = timeAfter - timeB4;
-		System.out.println("Linear search took: " + timeOfLinear + "ms");		
+		timeOfAction = timeAfter - timeB4;
+		System.out.println("Linear search took: " + timeOfAction + "ms");		
 		
-		System.out.println("\nBinary Search....");
+		System.out.println("\nArrays.Binary Search....");
 		
 		timeB4 = System.currentTimeMillis();
 		Arrays.sort(list3);
@@ -104,11 +133,29 @@ public class MainTester
 			position = -1;
 		}
 		System.out.println("Found at position " + position);
-		timeOfBinary = timeAfter - timeB4;
-		System.out.println("Binary search took: " + timeOfBinary + "ms" +
+		timeOfAction = timeAfter - timeB4;
+		System.out.println("Binary search took: " + timeOfAction + "ms" +
 				"\n+ " + timeOfSort + "ms for Arrays.sort() to prepare data for search.\nTotal time of binary: " 
-				+ (timeOfSort + timeOfBinary) + "ms");		
+				+ (timeOfSort + timeOfAction) + "ms");		
 		
+		System.out.println("\nBinary Search....");
+		
+		timeB4 = System.currentTimeMillis();
+		Arrays.sort(list4);
+		timeAfter = System.currentTimeMillis();
+		timeOfSort = timeAfter - timeB4;
+		timeB4 = System.currentTimeMillis();
+		position = BinarySearch.binarySearch(list4, 0, list4.length, searchNum);
+		timeAfter = System.currentTimeMillis();
+		if (position < 0)
+		{
+			position = -1;
+		}
+		System.out.println("Found at position " + position);
+		timeOfAction = timeAfter - timeB4;
+		System.out.println("Binary search took: " + timeOfAction + "ms" +
+				"\n+ " + timeOfSort + "ms for Arrays.sort() to prepare data for search.\nTotal time of binary: " 
+				+ (timeOfSort + timeOfAction) + "ms");		
 		}
 
 }
